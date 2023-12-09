7130e8b382a5bea9795fa0720d384ecc7088c965
Edwin <edwin.pile@controlrisks.com>
2023-12-09 16:13:45 -0500

    creating the structure

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
+import {AuthRoutes} from '../auth/routes/AuthRoutes'
+import {DashboardRoutes} from '../dashboard/routes/DashboardRoutes'
+import {HomepageRoutes} from '../homepage/routes/HomepageRoutes'
 
 export const AppRouter = () => {
   return (
     <Routes>
       {/* Login Y Registro*/}
-      <Route />
+      <Route path="/auth/*" element={<AuthRoutes />} />
 
       {/* Dashboard */}
-      <Route />
+      <Route path="/dashboard/*" element={<DashboardRoutes />} />
 
       {/* Landing Page */}
-      <Route />
+      <Route path="/*" element={<HomepageRoutes />} />
     </Routes>
   )
 }
