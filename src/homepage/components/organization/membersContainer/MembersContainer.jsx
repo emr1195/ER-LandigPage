8503a59453d3561d692ed111ce462024993d0a26
Edwin <edwin.pile@controlrisks.com>
2024-01-20 10:20:19 -0500

    amend on Organization

--- src/homepage/components/organization/membersContainer/MembersContainer.jsx
+++ src/homepage/components/organization/membersContainer/MembersContainer.jsx
@@ -1,23 +1,22 @@
-import {Box, Button, Tooltip, useTheme} from '@mui/material'
-import React, {useState} from 'react'
+import {Box, Tooltip, useTheme} from '@mui/material'
+import React from 'react'
 import {TypographyPersonalized} from '../../common'
 
 export const MembersContainer = ({members}) => {
   const theme = useTheme()
-  const [maxSlice, setMaxSlice] = useState(8)
 
   return (
     <Box display="flex" flexDirection="column" gap="32px" width="100%">
       <Box
         className="MembersContainer"
-        display="flex"
+        display={members?.coorNac ? 'flex' : 'none'}
         width="100%"
         justifyContent="center"
         alignItems="center"
         gap={{xs: '32px', laptop: '0'}}
         flexWrap="wrap"
       >
-        {members
+        {members?.coorNac
           .map((member, index) => {
             return (
               <Box
@@ -50,14 +49,14 @@ export const MembersContainer = ({members}) => {
                     },
                   }}
                 >
-                  <img src={member.image} alt={member.title} />
+                  <img src={member.image} alt={member.name} />
                 </Box>
                 <Box py="8px" display="flex" flexDirection="column">
-                  {member.title.length > 11 ? (
-                    <Tooltip title={member.title}>
+                  {member.role.length > 11 ? (
+                    <Tooltip title={member.role}>
                       <Box>
                         <TypographyPersonalized
-                          title={member.title.substring(0, 11) + '...'}
+                          title={member.role.substring(0, 11) + '...'}
                           color={theme.palette.black.main}
                           variant="body"
                           sx={{fontWeight: 'bold'}}
@@ -66,7 +65,7 @@ export const MembersContainer = ({members}) => {
                     </Tooltip>
                   ) : (
                     <TypographyPersonalized
-                      title={member.title}
+                      title={member.role}
                       color={theme.palette.black.main}
                       variant="body"
                       sx={{fontWeight: 'bold'}}
@@ -97,51 +96,283 @@ export const MembersContainer = ({members}) => {
               </Box>
             )
           })
-          .sort((a, b) => a.position - b.position)
-          .slice(0, maxSlice)}
+          .sort((a, b) => a.position - b.position)}
       </Box>
-      <Box
-        width="100%"
-        height={40}
-        py="32px"
-        position="relative"
-        alignItems="center"
-        justifyContent="center"
-        display={members.length > 8 ? 'flex' : 'none'}
-      >
-        <Box
-          sx={{
-            border: '1px solid #DADCE0',
-            width: '100%',
-            position: 'absolute',
-            left: 0,
-          }}
-        />
-        <Button
-          className={maxSlice > 8 ? 'MostrarMenos-Button' : 'MostrasMas-Button'}
-          onClick={
-            maxSlice > 8
-              ? () => setMaxSlice(8)
-              : () => setMaxSlice(members.length + 1)
-          }
-          sx={{
-            backgroundColor: '#f1f3f4',
-            borderRadius: '30px',
-            width: '300px',
-            height: '40px',
-            transition: 'all .5s',
 
-            '&:hover': {
-              backgroundColor: '#e0e0e3',
-            },
-          }}
-        >
-          <TypographyPersonalized
-            variant={'button'}
-            title={maxSlice > 8 ? 'Mostras menos' : 'Mostrar mas'}
-            sx={{fontWeight: 'bold'}}
-          />
-        </Button>
+      <Box
+        className="MembersContainer"
+        display={members?.comNac ? 'flex' : 'none'}
+        width="100%"
+        justifyContent="center"
+        alignItems="center"
+        gap={{xs: '32px', laptop: '0'}}
+        flexWrap="wrap"
+      >
+        {members?.comNac
+          .map((member, index) => {
+            return (
+              <Box
+                key={index}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  padding: '8px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: {
+                    xs: member.position == 0 ? '100%' : '140px',
+                    tablet: '140px',
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    width: '100%',
+                    height: '100%',
+                    margin: 'auto',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    '& img': {
+                      width: '120px',
+                      height: '180px',
+                      margin: 'auto',
+                      objectFit: 'cover',
+                    },
+                  }}
+                >
+                  <img src={member.image} alt={member.name} />
+                </Box>
+                <Box py="8px" display="flex" flexDirection="column">
+                  {member.role.length > 11 ? (
+                    <Tooltip title={member.role}>
+                      <Box>
+                        <TypographyPersonalized
+                          title={member.role.substring(0, 11) + '...'}
+                          color={theme.palette.black.main}
+                          variant="body"
+                          sx={{fontWeight: 'bold'}}
+                        />
+                      </Box>
+                    </Tooltip>
+                  ) : (
+                    <TypographyPersonalized
+                      title={member.role}
+                      color={theme.palette.black.main}
+                      variant="body"
+                      sx={{fontWeight: 'bold'}}
+                    />
+                  )}
+
+                  {member.name.length > 11 ? (
+                    <Tooltip
+                      title={member.name}
+                      sx={{display: member.name.length > 11}}
+                    >
+                      <Box>
+                        <TypographyPersonalized
+                          title={member.name.substring(0, 11) + '...'}
+                          variant="button"
+                          color={theme.palette.black50.main}
+                        />
+                      </Box>
+                    </Tooltip>
+                  ) : (
+                    <TypographyPersonalized
+                      title={member.name}
+                      variant="button"
+                      color={theme.palette.black50.main}
+                    />
+                  )}
+                </Box>
+              </Box>
+            )
+          })
+          .sort((a, b) => a.position - b.position)}
+      </Box>
+
+      <Box
+        className="MembersContainer"
+        display={members?.teamNac ? 'flex' : 'none'}
+        width="100%"
+        justifyContent="center"
+        alignItems="center"
+        gap={{xs: '32px', laptop: '0'}}
+        flexWrap="wrap"
+      >
+        {members?.teamNac
+          .map((member, index) => {
+            return (
+              <Box
+                key={index}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  padding: '8px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: {
+                    xs: member.position == 0 ? '100%' : '140px',
+                    tablet: '140px',
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    width: '100%',
+                    height: '100%',
+                    margin: 'auto',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    '& img': {
+                      width: '120px',
+                      height: '180px',
+                      margin: 'auto',
+                      objectFit: 'cover',
+                    },
+                  }}
+                >
+                  <img src={member.image} alt={member.name} />
+                </Box>
+                <Box py="8px" display="flex" flexDirection="column">
+                  {member.role.length > 11 ? (
+                    <Tooltip title={member.role}>
+                      <Box>
+                        <TypographyPersonalized
+                          title={member.role.substring(0, 11) + '...'}
+                          color={theme.palette.black.main}
+                          variant="body"
+                          sx={{fontWeight: 'bold'}}
+                        />
+                      </Box>
+                    </Tooltip>
+                  ) : (
+                    <TypographyPersonalized
+                      title={member.role}
+                      color={theme.palette.black.main}
+                      variant="body"
+                      sx={{fontWeight: 'bold'}}
+                    />
+                  )}
+
+                  {member.name.length > 11 ? (
+                    <Tooltip
+                      title={member.name}
+                      sx={{display: member.name.length > 11}}
+                    >
+                      <Box>
+                        <TypographyPersonalized
+                          title={member.name.substring(0, 11) + '...'}
+                          variant="button"
+                          color={theme.palette.black50.main}
+                        />
+                      </Box>
+                    </Tooltip>
+                  ) : (
+                    <TypographyPersonalized
+                      title={member.name}
+                      variant="button"
+                      color={theme.palette.black50.main}
+                    />
+                  )}
+                </Box>
+              </Box>
+            )
+          })
+          .sort((a, b) => a.position - b.position)}
+      </Box>
+
+      <Box
+        className="MembersContainer"
+        display={members?.cmDist ? 'flex' : 'none'}
+        width="100%"
+        justifyContent="center"
+        alignItems="center"
+        gap={{xs: '32px', laptop: '0'}}
+        flexWrap="wrap"
+      >
+        {members?.cmDist
+          .map((member, index) => {
+            return (
+              <Box
+                key={index}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  padding: '8px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: {
+                    xs: member.position == 0 ? '100%' : '140px',
+                    tablet: '140px',
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    width: '100%',
+                    height: '100%',
+                    margin: 'auto',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    '& img': {
+                      width: '120px',
+                      height: '180px',
+                      margin: 'auto',
+                      objectFit: 'cover',
+                    },
+                  }}
+                >
+                  <img src={member.image} alt={member.name} />
+                </Box>
+                <Box py="8px" display="flex" flexDirection="column">
+                  {member.role.length > 11 ? (
+                    <Tooltip title={member.role}>
+                      <Box>
+                        <TypographyPersonalized
+                          title={member.role.substring(0, 11) + '...'}
+                          color={theme.palette.black.main}
+                          variant="body"
+                          sx={{fontWeight: 'bold'}}
+                        />
+                      </Box>
+                    </Tooltip>
+                  ) : (
+                    <TypographyPersonalized
+                      title={member.role}
+                      color={theme.palette.black.main}
+                      variant="body"
+                      sx={{fontWeight: 'bold'}}
+                    />
+                  )}
+
+                  {member.name.length > 11 ? (
+                    <Tooltip
+                      title={member.name}
+                      sx={{display: member.name.length > 11}}
+                    >
+                      <Box>
+                        <TypographyPersonalized
+                          title={member.name.substring(0, 11) + '...'}
+                          variant="button"
+                          color={theme.palette.black50.main}
+                        />
+                      </Box>
+                    </Tooltip>
+                  ) : (
+                    <TypographyPersonalized
+                      title={member.name}
+                      variant="button"
+                      color={theme.palette.black50.main}
+                    />
+                  )}
+                </Box>
+              </Box>
+            )
+          })
+          .sort((a, b) => a.position - b.position)}
       </Box>
     </Box>
   )
